679bf9f414af00654ea49f652045f718bc9bc89e
Áron Gimesi <gimesiaron@gmail.com>
2023-12-04 22:59:29 +0100

    feat: cut and rotate image

--- Analyzer.py
+++ Analyzer.py
@@ -7,7 +7,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QGr
 from PyQt5.QtGui import QWindow, QPixmap, QImage
 from PyQt5.QtCore import Qt, QPoint, QRectF
 
-from utils import mask_white_objects, bbox_of_largest_white_object, convert_cv_to_qt, convert_qt_to_cv, dist
+from utils import BBox, convert_cv_to_qt, cut_straight_bbox_img, rotate_img
 
 
 class AnalyzerWindow(QMainWindow):
@@ -38,11 +38,23 @@ class AnalyzerWindow(QMainWindow):
 
     def cut_image(self):
         mask = self.bbox_editor.bbox.create_mask(self.img.shape).astype(bool)
+
         self.cut_img = self.img * mask
+
+        rotation = self.bbox_editor.bbox.get_rotation_angle(self.img.shape)
+
+        self.cut_img = cut_straight_bbox_img(
+            self.cut_img, self.bbox_editor.bbox)
 
         cv.imshow("N", self.cut_img)
         cv.waitKey(0)
         cv.destroyAllWindows()
+
+
+class CutImagePreviewWidget(QWidget):
+    def __init__(self, img):
+        super().__init__()
+        self.img = img
 
 
 class BBoxEditorWidget(QWidget):
@@ -107,59 +119,6 @@ class BBoxEditorWidget(QWidget):
         qt_image = convert_cv_to_qt(img)
         self.scene.clear()
         self.scene.addPixmap(qt_image)
-
-
-class BBox():
-    def __init__(self, tl, tr, bl, br):
-        self.tl = tl
-        self.tr = tr
-        self.bl = bl
-        self.br = br
-
-        self.edited_point_index = None
-
-    def __str__(self) -> str:
-        p = self.points()
-
-    def points(self):
-        return [self.tl, self.tr, self.bl, self.br]
-
-    def find_point_to_edit(self, point):
-        dists = [dist(point, p) for p in self.points()]
-        min_dist = np.array(dists).min()
-        min_dist_index = dists.index(min_dist)
-        self.edited_point_index = min_dist_index
-
-    def change_point(self, point):
-        print(point)
-        if self.edited_point_index == 0:
-            self.tl = point
-        elif self.edited_point_index == 1:
-            self.tr = point
-        elif self.edited_point_index == 2:
-            self.bl = point
-        elif self.edited_point_index == 3:
-            self.br = point
-
-        self.edited_point_index = None
-        self.__str__()
-
-    def create_mask(self, shape):
-        mask_img = np.zeros(shape, float)
-        points = np.array(self.points())
-
-        centroid = np.mean(points, axis=0)
-
-        # Calculate angles
-        angles = np.arctan2(points[:, 1] - centroid[1],
-                            points[:, 0] - centroid[0])
-
-        # Sort points based on angles
-        sorted_indices = np.argsort(angles)
-        sorted_points = points[sorted_indices]
-
-        cv.drawContours(mask_img, [sorted_points], -1, (1, 1, 1), -1)
-        return mask_img
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -3,6 +3,94 @@ import cv2 as cv
 from PyQt5.QtGui import QImage, QPixmap
 
 
+def bgr2rgb(img): return cv.cvtColor(img, cv.COLOR_BGR2RGB)
+def bgr2gray(img): return cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+def rgb2gray(img): return cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+def norm2int(img): return (img*255).astype(np.uint8)
+def norm2float(img): return (img/255.).astype(float)
+
+
+class BBox():
+    def __init__(self, tl, tr, bl, br):
+        self.tl = tl
+        self.tr = tr
+        self.bl = bl
+        self.br = br
+
+        self.edited_point_index = None
+
+    def __str__(self) -> str:
+        p = self.points()
+
+    def points(self):
+        return [self.tl, self.tr, self.bl, self.br]
+
+    def find_point_to_edit(self, point):
+        dists = [dist(point, p) for p in self.points()]
+        min_dist = np.array(dists).min()
+        min_dist_index = dists.index(min_dist)
+        self.edited_point_index = min_dist_index
+
+    def change_point(self, point):
+        print(point)
+        if self.edited_point_index == 0:
+            self.tl = point
+        elif self.edited_point_index == 1:
+            self.tr = point
+        elif self.edited_point_index == 2:
+            self.bl = point
+        elif self.edited_point_index == 3:
+            self.br = point
+
+        self.edited_point_index = None
+        self.__str__()
+
+    def create_mask(self, shape):
+        mask = np.zeros(shape, float)
+        points = np.array(self.points())
+
+        centroid = np.mean(points, axis=0)
+
+        angles = np.arctan2(points[:, 1] - centroid[1],
+                            points[:, 0] - centroid[0])
+
+        sorted_indices = np.argsort(angles)
+        sorted_points = points[sorted_indices]
+
+        cv.drawContours(mask, [sorted_points], -1, (1, 1, 1), -1)
+        return mask
+
+    def get_rotation_angle(self, shape):
+        mask = self.create_mask(shape)
+        mask = norm2int(mask)
+        mask = rgb2gray(mask)
+
+        cnts, hierarchy = cv.findContours(
+            mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cnt = cnts[0]
+
+        rect = cv.minAreaRect(cnt)
+        angle = rect[2]
+        return angle
+
+
+def cut_straight_bbox_img(img: np.ndarray, bbox: BBox):
+    mask = bbox.create_mask(img.shape)
+    angle = bbox.get_rotation_angle(img.shape)
+
+    rotated_mask = rotate_img(mask, -(90-angle))
+    rotated_img = rotate_img(img, -(90-angle))
+    rotated_masked_img = rotated_mask.astype(bool)*rotated_img
+
+    cnts, _ = cv.findContours(
+        rgb2gray(norm2int(rotated_mask)), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cnt = cnts[0]
+
+    x, y, w, h = cv.boundingRect(cnt)
+
+    return rotated_masked_img[y:y+h, x:x+w]
+
+
 def image2pixelmap(img: np.ndarray, shape=None):
     if shape is None:
         height, width, channel = img.shape
@@ -17,9 +105,9 @@ def image2pixelmap(img: np.ndarray, shape=None):
     return QPixmap.fromImage(image)
 
 
-def mask_white_objects(image):
+def mask_white_objects(img):
     # Convert the image to grayscale
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Threshold the grayscale image to create a binary mask for white objects
     _, thresholded = cv.threshold(gray, 200, 255, cv.THRESH_BINARY)
@@ -29,7 +117,7 @@ def mask_white_objects(image):
         thresholded, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
     # Create an empty mask
-    mask = np.zeros_like(image)
+    mask = np.zeros_like(img)
 
     for contour in contours:
         x, y, w, h = cv.boundingRect(contour)
@@ -37,13 +125,13 @@ def mask_white_objects(image):
                      (255, 255, 255), thickness=cv.FILLED)
 
     # Bitwise AND the original image with the mask to mask out white objects
-    masked_image = cv.bitwise_and(image, mask)
+    masked_image = cv.bitwise_and(img, mask)
 
     return masked_image
 
 
-def bbox_of_largest_white_object(image):
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+def bbox_of_largest_white_object(img):
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     _, thresholded = cv.threshold(gray, 200, 255, cv.THRESH_BINARY)
 
@@ -84,3 +172,11 @@ def convert_cv_to_qt(cv_image):
                       bytes_per_line, QImage.Format_RGB888)
     qt_image = qt_image.rgbSwapped()  # Fix color channels
     return QPixmap.fromImage(qt_image)
+
+
+def rotate_img(img, angle):
+    height, width = img.shape[:2]
+    rotation_matrix = cv.getRotationMatrix2D((width / 2, height / 2), angle, 1)
+    rotated_image = cv.warpAffine(
+        img, rotation_matrix, (width, height))
+    return rotated_image
